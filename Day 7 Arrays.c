a6dc77af2408838365538eda6960219ec481aff4
Dheeraj Chaudhary <38909585+dheeraj-2000@users.noreply.github.com>
2018-07-11 14:15:34 +0530

    Day 7 Solution in c

---
+++ Day 7 Arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int *arr = malloc(sizeof(int) * n);
+    for(int arr_i = 0; arr_i < n; arr_i++) {
+        scanf("%d", &arr[arr_i]);}
+    for(int arr_i = n; arr_i > 0; arr_i--) {
+        printf("%d ", arr[arr_i - 1]);}
+    printf("\n");
+    return 0;
+}
