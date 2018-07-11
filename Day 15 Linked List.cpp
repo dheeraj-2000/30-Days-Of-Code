a3e4618a175a5e81570487908d46671a14c18a49
Dheeraj Chaudhary <38909585+dheeraj-2000@users.noreply.github.com>
2018-07-11 14:32:04 +0530

    Day 15 solution in cpp

---
+++ Day 15 Linked List.cpp
@@ -0,0 +1,15 @@
+ Node* new_node;
++          new_node = new Node(data);
++          if (head == NULL){
++              head = new_node;
++          }
++          else {
++              Node* temp;
++              temp = head;
++              while (temp->next!=NULL){
++                  temp = temp->next;
++              }
++              temp->next = new_node;
++          }
++
++          return head;
