aa5fc5ad990f7c1ad20617cf78480dfade622507
Dheeraj Chaudhary <38909585+dheeraj-2000@users.noreply.github.com>
2018-07-11 13:46:01 +0530

    Day 3 solution in c

---
+++ Intro to Conditional Statements.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>0&&n<101)
+     { if(n%2==!0)
+      printf("Weird");
+         else if(n%2==0){
+            if(n>1&&n<6)
+             printf("Not Weird");
+            else if(n>5&&n<21)
+             printf("Weird");
+            else if(n>20)
+             printf("Not Weird"); }}
+
+    return 0;
+}
