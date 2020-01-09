f788c5f0aa076d0c5c2f3a035cda8e437866f4f3
Dheeraj Chaudhary <dheeraj12000@gmail.com>
2020-01-09 23:37:40 +0530

    Updated

--- Day 19 Interfaces.cpp
+++ Day 19 Interfaces.cpp
@@ -2,10 +2,8 @@ class Calculator : public AdvancedArithmetic {
 public:
     int divisorSum(int n) {
         int total=0,i;
-        for(i=1; i<=n; i++)
-        {
-            if(n%i == 0)
-            {
+        for(i=1; i<=n; i++){
+            if(n%i == 0) {
             total+=i;
             }
         }
