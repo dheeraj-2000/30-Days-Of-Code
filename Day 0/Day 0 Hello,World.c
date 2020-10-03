e7c7f3dab6ae8e97267ebb422e46ab9a55a4f6f0
dheeraj2000 <dheeraj12000@gmail.com>
2020-10-03 19:00:12 +0530

    updated folder of Day

---
+++ Day 0/Day 0 Hello,World.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    char input_string[105];
+    scanf("%[^\n]", input_string);
+    printf("Hello, World.\n");
+    printf("%s", input_string);
+    return 0;
+}
+
