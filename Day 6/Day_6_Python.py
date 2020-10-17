2655c44aa2e145c54e64d513216e51940cbffc56
Serena Raju <53613655+serenaraju@users.noreply.github.com>
2020-10-17 10:46:46 +0530

    "Day 6 Python with Time and Space Complexity"

--- Day 6/Day_6_Python.py
+++ Day 6/Day_6_Python.py
@@ -2,8 +2,11 @@
 
 # Let's review
 
-for i in range(int(input())):
+for _ in range(int(input())):
     w = input() # Input word
     print(*(''.join(w[::2]),''.join(w[1::2])))
 
 # Using slicing of a string we can achieve the result in minimal lines of code
+
+# Time complexity: O(n) Because only 1 for loop is iterated n times
+# Space complexity: O(n) Because linear heap space is required for the input string
